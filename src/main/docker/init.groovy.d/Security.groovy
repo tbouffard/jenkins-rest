41b96312efa20800be1e3d7eda3036e7dfa8402a
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2019-10-11 19:52:17 +0200

    docker: add admin user

--- src/main/docker/init.groovy.d/Security.groovy
+++ src/main/docker/init.groovy.d/Security.groovy
@@ -1,7 +1,30 @@
+import hudson.security.HudsonPrivateSecurityRealm
+import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
 import hudson.security.csrf.DefaultCrumbIssuer
+
 import jenkins.model.Jenkins
 
 def instance = Jenkins.instanceOrNull
 
+// =====================================================================================================================
+// Enable CSRF protection (see: https://wiki.jenkins.io/display/JENKINS/CSRF+Protection)
+// =====================================================================================================================
 instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
+
+
+// =====================================================================================================================
+// Create the admin user
+// =====================================================================================================================
+
+def jenkinsRealm = new HudsonPrivateSecurityRealm(false)
+jenkinsRealm.createAccount('admin', 'admin')
+instance.setSecurityRealm(jenkinsRealm)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(true)
+instance.setAuthorizationStrategy(strategy)
+
+
+// =====================================================================================================================
+// Save everything
+// =====================================================================================================================
 instance.save()
