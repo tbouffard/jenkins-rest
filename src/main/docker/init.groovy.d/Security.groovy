55866f197e7ad1621ccf2b9a738e26ed432f27a9
Thomas Bouffard <thomas.bouffard@bonitasoft.com>
2019-10-15 14:18:55 +0200

    load plugins + configure antisamy markup formatter

--- src/main/docker/init.groovy.d/Security.groovy
+++ src/main/docker/init.groovy.d/Security.groovy
@@ -1,10 +1,16 @@
+import hudson.markup.RawHtmlMarkupFormatter
 import hudson.security.HudsonPrivateSecurityRealm
 import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
 import hudson.security.csrf.DefaultCrumbIssuer
-
 import jenkins.model.Jenkins
 
 def instance = Jenkins.instanceOrNull
+
+// =====================================================================================================================
+// Allow html markup with syntax highlighting
+// =====================================================================================================================
+instance.setMarkupFormatter(new RawHtmlMarkupFormatter(false))
+
 
 // =====================================================================================================================
 // Enable CSRF protection (see: https://wiki.jenkins.io/display/JENKINS/CSRF+Protection)
